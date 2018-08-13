b6e6c6be85ed98b704e8e62a92582843b0d606b7
BrEaLSxx7 <sebascano65@gmail.com>
2018-08-13 12:39:36 -0500

    ..

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit } from "@angular/core";
+import * as Chartist from "chartist";
+
+@Component({
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
+})
+export class DashboardComponent implements OnInit {
+  public chartColor;
+  public canvas: any;
+  public ctx;
+  public gradientFill;
+  public lineBigDashboardChartType;
+  public lineBigDashboardChartData: Array<any>;
+  public lineBigDashboardChartOptions: any;
+  public lineBigDashboardChartLabels: Array<any>;
+  public lineBigDashboardChartColors: Array<any>;
+  constructor() {}
+
+  ngOnInit() {
+    this.chartColor = "#FFFFFF";
+    this.canvas = document.getElementById("bigDashboardChart");
+    this.ctx = this.canvas.getContext("2d");
+
+    this.gradientFill = this.ctx.createLinearGradient(0, 200, 0, 50);
+    this.gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+    this.gradientFill.addColorStop(1, "rgba(255, 255, 255, 0.24)");
+    this.lineBigDashboardChartData = [
+      {
+        label: "# de pedidos",
+
+        pointBorderWidth: 1,
+        pointHoverRadius: 7,
+        pointHoverBorderWidth: 2,
+        pointRadius: 5,
+        fill: true,
+
+        borderWidth: 2,
+        data: [50, 150, 100, 190]
+      }
+    ];
+
+    this.lineBigDashboardChartColors = [
+      {
+        backgroundColor: this.gradientFill,
+        borderColor: this.chartColor,
+        pointBorderColor: this.chartColor,
+        pointBackgroundColor: "#2c2c2c",
+        pointHoverBackgroundColor: "#2c2c2c",
+        pointHoverBorderColor: this.chartColor
+      }
+    ];
+
+    this.lineBigDashboardChartLabels = [
+      "Sucursal #",
+      "Sucursal #",
+      "Sucursal #",
+      "Sucursal #",
+    ];
+    this.lineBigDashboardChartOptions = {
+      layout: {
+        padding: {
+          left: 20,
+          right: 20,
+          top: 0,
+          bottom: 0
+        }
+      },
+      maintainAspectRatio: false,
+      tooltips: {
+        backgroundColor: "#fff",
+        titleFontColor: "#333",
+        bodyFontColor: "#666",
+        bodySpacing: 4,
+        xPadding: 12,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest"
+      },
+      legend: {
+        position: "bottom",
+        fillStyle: "#FFF",
+        display: false
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              fontColor: "rgba(255,255,255,0.4)",
+              fontStyle: "bold",
+              beginAtZero: true,
+              maxTicksLimit: 5,
+              padding: 10
+            },
+            gridLines: {
+              drawTicks: true,
+              drawBorder: false,
+              display: true,
+              color: "rgba(255,255,255,0.1)",
+              zeroLineColor: "transparent"
+            }
+          }
+        ],
+        xAxes: [
+          {
+            gridLines: {
+              zeroLineColor: "transparent",
+              display: false
+            },
+            ticks: {
+              padding: 10,
+              fontColor: "rgba(255,255,255,0.4)",
+              fontStyle: "bold"
+            }
+          }
+        ]
+      }
+    };
+
+    this.lineBigDashboardChartType = "bar";
+  }
+}
